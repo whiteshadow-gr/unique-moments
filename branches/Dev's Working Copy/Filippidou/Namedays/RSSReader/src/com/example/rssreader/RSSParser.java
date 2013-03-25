4cc746ca030f89996714980339a4ca5573044009
ellada_f@hotmail.com <ellada_f@hotmail.com@e96e017e-f7f1-33b6-b2f0-7ed09530838d>
2013-03-25 20:59:35 +0000

    final

---
+++ branches/Dev's Working Copy/Filippidou/Namedays/RSSReader/src/com/example/rssreader/RSSParser.java
@@ -0,0 +1,95 @@
+package com.example.rssreader;
+
+import java.util.ArrayList;
+ 
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+ 
+public class RSSParser extends DefaultHandler {
+        private final static String TAG_ITEM = "item";
+        private final static String[] xmltags = { "title", "link", "pubDate", "description" };
+         
+        private RSSItem currentitem = null;
+        private ArrayList<RSSItem> itemarray = null;
+        private int currentindex = -1;
+        private boolean isParsing = false;
+        private StringBuilder builder = new StringBuilder();
+         
+        public RSSParser(ArrayList<RSSItem> itemarray) {
+                super();
+                 
+                this.itemarray = itemarray;
+        }
+ 
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+                super.characters(ch, start, length);
+                 
+                if(isParsing && -1 != currentindex && null != builder)
+                {
+                        builder.append(ch,start,length);
+                }
+        }
+                 
+        @Override
+        public void startElement(String uri, String localName, String qName,Attributes attributes) throws SAXException {
+                super.startElement(uri, localName, qName, attributes);
+                //sunartisi i opoia anoigei to kathe stoixeiou(element) pou diavazei sto xml arxeio.
+                
+                if(localName.equalsIgnoreCase(TAG_ITEM))
+                {
+                        currentitem = new RSSItem();
+                        currentindex = -1;
+                        isParsing = true;
+                         
+                        itemarray.add(currentitem);
+                }
+                else
+                {
+                        currentindex = itemIndexFromString(localName);
+                         
+                        builder = null;
+                         
+                        if(-1 != currentindex)
+                                builder = new StringBuilder();
+                }
+        }
+         
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+                super.endElement(uri, localName, qName);
+            //sunartisi i opoia pernei ta dedomena apo to kathe stoixeio(element)      
+                if(localName.equalsIgnoreCase(TAG_ITEM))
+                {
+                        isParsing = false;
+                }
+                else if(currentindex != -1)
+                {
+                        if(isParsing)
+                        {
+                                switch(currentindex)
+                                {
+                                        case 0: currentitem.title = builder.toString();                 break;
+                                        case 1: currentitem.link = builder.toString();                  break;
+                                }
+                        }
+                }
+        }
+ 
+        private int itemIndexFromString(String tagname){
+                int itemindex = -1;
+ 
+                for(int index= 0; index<xmltags.length; ++index)
+                {
+                        if(tagname.equalsIgnoreCase(xmltags[index]))
+                        {
+                                itemindex = index;
+                                 
+                                break;
+                        }
+                }
+                 
+                return itemindex;
+        }
+}
---
+++ branches/Dev's Working Copy/Filippidou/Namedays/RSSReader/src/com/example/rssreader/RssReaderActivity.java
@@ -0,0 +1,158 @@
+package com.example.rssreader;
+ 
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+ 
+public class RssReaderActivity extends ListActivity {
+    private ArrayList<RSSItem> itemlist = null;
+    private RSSListAdaptor rssadaptor = null;
+     
+@Override
+public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+     
+    itemlist = new ArrayList<RSSItem>();
+     
+    new RetrieveRSSFeeds().execute();
+}
+ 
+ 
+    private void retrieveRSSFeed(String urlToRssFeed,ArrayList<RSSItem> list)
+{
+    	//sunartisi i opoia ulopoiei tin sundesi sto rssfeed.
+    try
+    {
+       URL url = new URL(urlToRssFeed);
+       SAXParserFactory factory = SAXParserFactory.newInstance();
+       SAXParser parser = factory.newSAXParser();
+       XMLReader xmlreader = parser.getXMLReader();
+       RSSParser theRssHandler = new RSSParser(list);
+ 
+       xmlreader.setContentHandler(theRssHandler);
+ 
+       InputSource is = new InputSource(url.openStream());
+ 
+       xmlreader.parse(is);
+    }
+    catch (Exception e)
+    {
+        e.printStackTrace();
+    }
+}
+ 
+private class RetrieveRSSFeeds extends AsyncTask<Void, Void, Void>
+{
+	//ektelesi tis sundesis sto rssfeed me tin voitheia tou AsyncTask i opoia ektelei tin diadikasia sto background
+    private ProgressDialog progress = null;
+     
+            @Override
+            protected Void doInBackground(Void... params) {
+                    retrieveRSSFeed("http://www.eortologio.gr/rss/si_en.xml",itemlist);
+                   
+                    rssadaptor = new RSSListAdaptor(RssReaderActivity.this, R.layout.activity_rss_reader,itemlist);
+                     
+                    return null;
+            }
+     
+            @Override
+            protected void onCancelled() {
+                    super.onCancelled();
+            }
+             
+            @Override
+            protected void onPreExecute() {
+                    progress = ProgressDialog.show(
+                            RssReaderActivity.this, null, "Loading RSS Feeds...");
+                     
+                    super.onPreExecute();
+            }
+             
+            @Override
+            protected void onPostExecute(Void result) {
+                    setListAdapter(rssadaptor);
+                     
+                    progress.dismiss();
+                     
+                    super.onPostExecute(result);
+            }
+             
+            @Override
+            protected void onProgressUpdate(Void... values) {
+                    super.onProgressUpdate(values);
+            }
+}
+ 
+private class RSSListAdaptor extends ArrayAdapter<RSSItem>{
+	
+	//emfanisei twn apoelesmatwn me tin voitheia tou adapter
+	
+    private List<RSSItem> objects = null;
+     
+            public RSSListAdaptor(Context context, int textviewid, List<RSSItem> objects) {
+                    super(context, textviewid, objects);
+                     
+                    this.objects = objects;
+            }
+             
+            @Override
+            public int getCount() {
+                    return ((null != objects) ? objects.size() : 0);
+            }
+             
+            @Override
+            public long getItemId(int position) {
+                    return position;
+            }
+             
+            @Override
+            public RSSItem getItem(int position) {
+                    return ((null != objects) ? objects.get(position) : null);
+            }
+             
+            public View getView(int position, View convertView, ViewGroup parent) {
+                    View view = convertView;
+                     
+                    if(null == view)
+                    {
+                            LayoutInflater vi = (LayoutInflater)RssReaderActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                            view = vi.inflate(R.layout.activity_rss_reader, null);
+                    }
+                     
+                    RSSItem data = objects.get(position);
+                     
+                    if(null != data)
+                    {
+                        
+                           TextView names = (TextView)view.findViewById(R.id.textView1);
+                           
+                           String tmp[];
+                           tmp=data.title.split("\\(");
+                           
+                           names.setText(tmp[0]);
+                        
+                    }
+                     
+                    return view;
+            }
+}
+}
