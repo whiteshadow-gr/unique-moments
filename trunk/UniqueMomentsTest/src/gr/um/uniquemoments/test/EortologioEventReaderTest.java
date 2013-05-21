fc2580a09ceab4b5bdea2fb5e6c068ef70792da0
vmbouras@gmail.com <vmbouras@gmail.com@e96e017e-f7f1-33b6-b2f0-7ed09530838d>
2013-05-21 14:47:15 +0000

    Updating Trunk with latest

---
+++ trunk/UniqueMomentsTest/src/gr/um/uniquemoments/test/EortologioEventReaderTest.java
@@ -0,0 +1,46 @@
+package gr.um.uniquemoments.test;
+
+import gr.um.entities.RSSItem;
+import gr.um.eortologio.EortologioEventReader;
+
+import java.util.ArrayList;
+
+import android.test.InstrumentationTestCase;
+
+public class EortologioEventReaderTest extends InstrumentationTestCase 
+{
+	private EortologioEventReader eortologio;
+	
+	protected void setUp() throws Exception 
+	{
+		super.setUp();
+		eortologio = new EortologioEventReader("http://www.eortologio.gr/rss/si_en.xml");
+	}
+
+	
+
+	public void testGetNames() 
+	{
+		
+		ArrayList <String> expected = new ArrayList <String>();
+		expected.add("Glykeria");
+	
+		ArrayList <String> actual=new ArrayList <String>();
+		actual = eortologio.getNames();
+		
+		
+		assertEquals(actual,expected);
+	}
+	
+	public void testRetrieveRSSFeed()
+	{
+		ArrayList <RSSItem> listActual = new ArrayList<RSSItem>();
+		
+		listActual = eortologio.retrieveRSSFeed(listActual);
+		
+		String expected = "today 13/5 :  Glykeria (source : www.namedays.gr)";
+		String actual = listActual.get(0).title;
+		
+		assertEquals(expected, actual);
+	}
+}
