606fe1066b2331314c0dabb2eefc32b8008c7073
vmbouras@gmail.com <vmbouras@gmail.com@e96e017e-f7f1-33b6-b2f0-7ed09530838d>
2013-05-25 18:08:19 +0000

    Deleting Double branches Entries

--- Filippidou/Namedays/RssReader/src/com/example/rssreader/RSSItem.java
+++
@@ -1,8 +0,0 @@
-package com.example.rssreader;
- 
-public class RSSItem {
-	
-    public String title;
-    public String link;
-   
-}
--- Filippidou/Namedays/eortologioDB/gen/com/example/eortologiodb/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.eortologiodb;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
