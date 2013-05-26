3122b27ba54eb897d10f7d1de82d2e68e770d4ea
tpaito@yahoo.com <tpaito@yahoo.com@e96e017e-f7f1-33b6-b2f0-7ed09530838d>
2013-05-26 00:38:09 +0000

    Description: This is the actually working version! Author: Theodoros Paitotsoglou Commition Date: 26/5/2013

--- branches/Dev's Working Copy/Paitotsoglou/UniqueMoments/src/gr/um/email/EmailSender.java
+++ branches/Dev's Working Copy/Paitotsoglou/UniqueMoments/src/gr/um/email/EmailSender.java
@@ -6,7 +6,10 @@ import android.content.Intent;
 
 public class EmailSender implements IEmail
 {
-	Activity activityEmail;
+	public Activity activityEmail;
+	private String whereto;
+	private String MailHeader;
+	private String ActualMessage;
 	
 	public EmailSender(Activity act)
 	{
@@ -15,6 +18,22 @@ public class EmailSender implements IEmail
 	
 	public void sendEmail(Intent emailIntent)
 	{
+		MailHeader = emailIntent.EXTRA_SUBJECT;
+		ActualMessage = emailIntent.EXTRA_TEXT;
+		whereto = emailIntent.EXTRA_EMAIL;
 		activityEmail.startActivity(Intent.createChooser(emailIntent, "Email"));
 	}
+
+	@Override
+	public String[] getIntentData() 
+	{
+		// TODO Auto-generated method stub
+		String[] data = new String[3];
+		data[0] = MailHeader;
+		data[1] = ActualMessage;
+		data[2] = whereto;
+		
+		return data;
+	}
 }
+
