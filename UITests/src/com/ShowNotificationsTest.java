42df16910f5d99f76e545d53ec22266419242ab7
vmbouras@gmail.com <vmbouras@gmail.com@e96e017e-f7f1-33b6-b2f0-7ed09530838d>
2013-05-28 11:17:02 +0000

    Final work of Tester's

---
+++ UITests/src/com/ShowNotificationsTest.java
@@ -0,0 +1,58 @@
+package com;
+
+import com.android.uiautomator.core.UiCollection;
+import com.android.uiautomator.core.UiObjectNotFoundException;
+import com.android.uiautomator.core.UiSelector;
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+public class ShowNotificationsTest extends UiAutomatorTestCase {
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}	
+	
+	
+	public void testExistingContactAlfaios() throws UiObjectNotFoundException{
+		getUiDevice().click(100,100);
+		UiCollection NotificationsList = new UiCollection(new UiSelector().className("android.widget.ImageView") );
+		int AllContacts = NotificationsList.getChildCount();
+		int exp = 1;
+		assertEquals(exp , AllContacts);
+	}
+	
+	public void testExistingContactSinesios() throws UiObjectNotFoundException{
+		getUiDevice().click(100,100);
+		UiCollection NotificationsList = new UiCollection(new UiSelector().className("android.widget.ImageView") );
+		int AllContacts = NotificationsList.getChildCount();
+		int exp = 1;
+		assertEquals(exp , AllContacts);
+	}
+	
+	public void testExistingContactAleksandros() throws UiObjectNotFoundException{
+		getUiDevice().click(100,100);
+		UiCollection NotificationsList = new UiCollection(new UiSelector().className("android.widget.ImageView") );
+		int AllContacts = NotificationsList.getChildCount();
+		int exp = 0;
+		assertEquals(exp , AllContacts);
+	}
+	
+	public void testExistingContactElenh() throws UiObjectNotFoundException{
+		getUiDevice().click(100,100);
+		UiCollection NotificationsList = new UiCollection(new UiSelector().className("android.widget.ImageView") );
+		int AllContacts = NotificationsList.getChildCount();
+		int exp = 0;
+		assertEquals(exp , AllContacts);
+	}
+	
+	public void testExistingContactStefanos() throws UiObjectNotFoundException{
+		getUiDevice().click(100,100);
+		UiCollection NotificationsList = new UiCollection(new UiSelector().className("android.widget.ImageView") );
+		int AllContacts = NotificationsList.getChildCount();
+		int exp = 0;
+		assertEquals(exp , AllContacts);
+	}
+}
