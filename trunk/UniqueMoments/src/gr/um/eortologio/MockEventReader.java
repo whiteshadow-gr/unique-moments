b34e5e8c1f54e43503e13ded36d27d73891f7dab
vmbouras@gmail.com <vmbouras@gmail.com@e96e017e-f7f1-33b6-b2f0-7ed09530838d>
2013-05-28 12:27:43 +0000

    Last minute file changes from Team Developers

--- trunk/UniqueMoments/src/gr/um/eortologio/MockEventReader.java
+++ trunk/UniqueMoments/src/gr/um/eortologio/MockEventReader.java
@@ -3,21 +3,69 @@ package gr.um.eortologio;
 import gr.um.entities.RSSItem;
 import gr.um.interfaces.ICelebrationEventReader;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
-public class MockEventReader implements ICelebrationEventReader 
+import android.content.Context;
+import android.util.Log;
+
+public class MockEventReader implements ICelebrationEventReader
 {
+        Context context= null;
+       
+        public MockEventReader(Context context)
+        {
+                this.context=context;
+               
+        }
+        @Override
+        public ArrayList<String> getRSSNames()
+        {
+                ArrayList <RSSItem> namesRSS = new ArrayList <RSSItem>();
+                ArrayList <String> names = new ArrayList <String>();
+               
+                namesRSS = retrieveRSSFeed(namesRSS);
+        namesRSS.get(0).generateArrayListNames();
+       
+        String [] namesArrayRSS = namesRSS.get(0).names;
+        for (int i = 0; i < namesArrayRSS.length; i++)
+        {
+                names.add(namesArrayRSS[i]);
+        }
+        return names;
+        }
 
-	@Override
-	public ArrayList<String> getRSSNames() 
-	{
-		// TODO Auto-generated method stub
-		return new ArrayList<String>();
-	}
+        @Override
+        public ArrayList<RSSItem> retrieveRSSFeed(ArrayList<RSSItem> list) {
+                String xml = null;
+                 try{
+                 xml=getXML(context.getAssets().open("MockRssFeed.xml"));
+                 }catch(Exception e){
+                     Log.d("Error",e.toString());
+                  }
+                 
+                 RSSItem item = new RSSItem();
+                 item.setTitle(xml);
+                 
+                 list.add(item);
+                 
+                 return list;
+        }
+       
+        //getXML   method
+         public static String getXML(InputStream is)throws IOException {
 
-	@Override
-	public ArrayList<RSSItem> retrieveRSSFeed(ArrayList<RSSItem> list) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+            BufferedInputStream bis = new BufferedInputStream(is);
+            ByteArrayOutputStream buf = new ByteArrayOutputStream();
+            int result = bis.read();
+            while(result != -1) {
+              byte b = (byte)result;
+              buf.write(b);
+              result = bis.read();
+            }        
+            return buf.toString();
+        }
 }
